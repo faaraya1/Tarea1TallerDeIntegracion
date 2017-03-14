4b4e7576a1c4449e527c94ede05ede4bc1d69fec
Fernando <faaraya1@uc.cl>
2017-03-13 23:42:09 -0300

    algunos avances en el proyecto

---
+++ noticias/app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+	#GET /articles
+	def index
+		@articles = Article.all
+	end
+	#GET /articles/:id
+	def show
+		@article = Article.find(params[:id])
+	end
+	#GET /articles/show
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+	#POST /articles
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			 render :new
+		end	
+	end
+	#DELETE /articles/:id
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path		
+	end
+	#PUT /articles/:id
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			 render :edit
+		end	
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :subhead, :body)
+		
+	end
+
+end
--- noticias/app/models/article.rb
+++ noticias/app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+	validates :title, presence: true, uniqueness: true 
+	validates :subhead, presence: true, length: {maximum: 250} 
+	validates :body, presence: true
 end
