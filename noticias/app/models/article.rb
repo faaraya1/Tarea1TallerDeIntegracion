80e82df21e325330313ec74defa0cd3a8ff41670
Fernando <faaraya1@uc.cl>
2017-03-14 01:41:36 -0300

    comentarios listos. me falta solo orden inverso y truncar los caracteres

--- noticias/app/models/article.rb
+++ noticias/app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+	has_many :comments
 	validates :title, presence: true, uniqueness: true 
 	validates :subhead, presence: true, length: {maximum: 250} 
 	validates :body, presence: true
