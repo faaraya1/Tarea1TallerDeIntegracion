1ec35b28db15967f8e8fe64897551ac35c140114
Fernando <faaraya1@uc.cl>
2017-03-14 03:10:14 -0300

    todo listo. falta migrarlo a postgres y subirlo y agregar el mensaje de confirmacion

--- noticias/app/models/article.rb
+++ noticias/app/models/article.rb
@@ -1,9 +1,12 @@
 class Article < ApplicationRecord
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, uniqueness: true 
 	validates :subhead, presence: true, length: {maximum: 250} 
 	validates :body, presence: true
 	def body_truncated
 		body.truncate(1003)
-	end
+	end	
+	#def formatted_created_at
+	#	(created_at.to_time - 3.hours).to_time
+	#end
 end
