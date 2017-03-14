9da405c9c42e95fd491d2d70ef2e5946fa777845
Fernando <faaraya1@uc.cl>
2017-03-14 02:14:54 -0300

    todo listo. falta migrarlo a postgres y subirlo. ademas me faltaria arreglar las fechas

--- noticias/app/models/article.rb
+++ noticias/app/models/article.rb
@@ -3,4 +3,7 @@ class Article < ApplicationRecord
 	validates :title, presence: true, uniqueness: true 
 	validates :subhead, presence: true, length: {maximum: 250} 
 	validates :body, presence: true
+	def body_truncated
+		body.truncate(1003)
+	end
 end
